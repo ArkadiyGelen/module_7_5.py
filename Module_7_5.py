18aad14a327e83fbbfb9d7a5287c65866480af0a
ArkadiyGelen <ArkadijTolstov@gmail.com>
2024-12-03 12:00:44 +0300

    Update Module_7_5.py

--- Module_7_5.py
+++ Module_7_5.py
@@ -5,8 +5,8 @@ directory = '.'
 
 for root, dirs, files in os.walk(directory):
   for file in files:
-    filepath = os.path.join(file)
-    filetime = os.path.getmtime(file)
+    filepath = os.path.join(root, file)
+    filetime = os.path.getmtime(filepath)
     formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
     file_size = os.path.getsize(file)
     parent_dir = os.path.dirname(file)
